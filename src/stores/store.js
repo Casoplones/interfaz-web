4d0f4a45917dda03a297b8226d137d4298aef5a5
Alvaro <alvarobermejo2004@gmail.com>
2024-11-22 09:34:02 +0100

    Implementar store de tareas con funciones para listar, agregar, actualizar y eliminar tareas

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,4 +1,32 @@
-import { writable } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
+
+function tareasStore(){
+  const { subscribe, set, update} = writable([]);
+
+  return{
+    subscribe, 
+
+    listarTareas: () => update(tareas => tareas),
+
+    agregarTarea: (tarea) => update(tareas => {
+      const nuevaTarea = { id: tareas.length + 1, ...tarea, estado: false };
+      return [...tareas, nuevaTarea];
+    }),
+
+    actualizarTarea: (id, tarea) => update(tareas => {
+      const tareaEditada = tareas.map(tarea => tarea.id === id ? {...tarea, ...tarea} : tarea);
+      return tareaEditada;
+    }),
+
+    eliminarTarea: (id) => update(tareas => tareas.filter(tarea => tarea.id !== id)),
+
+    }
+
+    reset : () => set([])
+}
+
+export const listaTareas = tareasStore();
+export const numeroClientes = derived(listaTareas, ($listaTareas) => $listaTareas.length);
 
 export let count = writable(0);
 export let tareas = writable([]);
